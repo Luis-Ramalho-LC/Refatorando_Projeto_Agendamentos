e75303291fcda8afd7ee5bd8bb3298356dffc911
Luís Ramalho <luisramalholc@gmail.com>
2026-03-18 14:34:31 -0300

    preparação para o inicio do projeto

---
+++ projeto-agendamentos-react/src/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header.js'
+import HomePage from './components/HomePage.js'
+import Footer from './components/Footer.js'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ projeto-agendamentos-react/src/components/Header.js
@@ -0,0 +1,31 @@
+export default function Header(){
+    return(
+        <header>
+          <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.html">Página inicial</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="crudAgendamento.html">Gerenciar Serviços</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="agendarHorarios.html">Agendamentos</a>
+                  </li>
+                </ul>
+                <form class="d-flex" role="search">
+                  <input id="SearchIndex" class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+        </header>
+    )
+}
